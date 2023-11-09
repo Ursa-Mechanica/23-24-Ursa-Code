63a2112dd6d73bf6fe0026bef9fe40df57579c3b
MagicalBean <ben.weisz13@gmail.com>
2023-11-08 21:00:50 -0800

    Converted into vanilla java

--- InverseKinematics.java
+++ InverseKinematics.java
@@ -1,29 +1,47 @@
-// Target point for end effector
-float targetX = 3.3;
-float targetY = 4;
+import java.lang.Math;
 
-// Lengths of each arm segment
-float len1 = 5.3 * 2;
-float len2 = 2.58 * 2;
-float len3 = 1;
+public class IK
+{
+    double len1 = 10.6;
+    double len2 = 5.16;
+    double len3 = 1;
 
-// Angle relative to +X for end effector
-float phi = radians(0);
+    double phi = 0;
 
-// Angle for each segment
-float theta1, theta2, theta3 = 0;
+    double[] run(double targetX, double targetY)
+    {
+        double[] angles = new double[3];
 
-// Point at joint between segments 2 and 3
-float wx = targetX - len3 * cos(phi);
-float wy = targetY - len3 * sin(phi);  
+        double wx = targetX - len3 * Math.cos(phi);
+        double wy = targetY - len3 * Math.sin(phi);  
+    
+        double delta = wx * wx + wy * wy;
+        double phi1 = Math.acos((len1 * len1 + delta - len2 * len2) / (2 * len1 * Math.sqrt(delta)));
+        double phi2 = Math.atan2(wy, wx);
+        angles[0] = phi1 + phi2;
+        
+        double phi3 = Math.acos((len1 * len1 + len2 * len2 - delta) / (2 * len1 * len2));
+        angles[1] = Math.PI + phi3;
+        
+        angles[2] = phi - angles[0] - angles[1];
 
-// Law of Cosines
-float delta = sq(wx) + sq(wy);
-float phi1 = acos((sq(len1) + delta - sq(len2)) / (2 * len1 * sqrt(delta)));
-float phi2 = atan2(wy, wx);
-theta1 = phi1 + phi2;
+        return remapValues(angles);
+    }
 
-float phi3 = acos((sq(len1) + sq(len2) - delta) / (2 * len1 * len2));
-theta2 = radians(180) + phi3;
+    double[] remapValues(double[] angles)
+    {
+        // ostart + (ostop - ostart) * ((value - istart) / (istop - istart))
+        angles[0] = 1 - (angles[0] / (Math.PI / 2));
+        angles[0] = Math.round(angles[0] * 100.0) / 100.0;
+        angles[1] = (angles[1] - Math.toRadians(225)) / (Math.toRadians(325) - Math.toRadians(225));
+        angles[1] = Math.round(angles[1] * 100.0) / 100.0;
 
-theta3 = phi - theta1 - theta2;
+        return angles;
+    }
+
+    public static void main(String[] args) {
+        IK ik = new IK();
+        double[] angles = ik.run(8.2024, 1.628225);
+		System.out.println(angles[0] + ", " + angles[1] + ", " + angles[2]);
+	}
+}
