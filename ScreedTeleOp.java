15b0ec1f8b5da155f62f06f6ea24be56ef4db151
MagicalBean <ben.weisz13@gmail.com>
2024-02-26 08:43:00 -0800

    Added hanging controls ScreedTeleOp.java

--- ScreedTeleOp.java
+++ ScreedTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
@@ -19,27 +20,48 @@ public class ScreedTeleOp extends LinearOpMode {
     public ElapsedTime runtime = new ElapsedTime();
     public ElapsedTime debounce = new ElapsedTime();
     
-    public int intakeToggle = 0;
+    public int intakeToggle, spinPos = 0;
+    public double wristPos = 0.5;
+    public double[] spinPositions = new double[]{0.175, 0.3725, 0.57};
+    public double spinLeft = 0.175;
+    public double spinRight = 0.57;
+    public double armPos = 0;
     
+    public boolean isArmUp = false;
+    
+    double a = 0;
+    double b = -1883.05;
+    
+    boolean spinPressed = false;
     boolean grabberOpen = false;
     boolean grabberPressed = false;
+    boolean enableLifting = false;
+    boolean liftToggle = false;
 
     @Override
     public void runOpMode() {
         robot.init(hardwareMap);
+        a = (2.1071f * Math.pow(10, -8));
         
         waitForStart();
         runtime.reset();
 
-        double originPoint = 0;
+        double originPoint = robot.m1.getCurrentPosition();
+        
+        wristPos = 0.53;
 
         while (opModeIsActive()) {
             
             if (robot.limit.isPressed()) originPoint = robot.m1.getCurrentPosition();
             
-            telemetry.addData("Origin", originPoint);
-            telemetry.addData("Current", robot.m1.getCurrentPosition());
+            armPos = robot.m1.getCurrentPosition();
             
+            telemetry.addData("Origin", originPoint);
+            telemetry.addData("Current", originPoint - robot.m1.getCurrentPosition());
+            telemetry.addData("Wrist", robot.wrist.getPosition());
+            telemetry.addData("Spinner", robot.spinner.getPosition());
+            
+            // ----- DRIVE CODE -----
             double rx = gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x;
             double y = -gamepad1.right_stick_x;
@@ -56,6 +78,8 @@ public class ScreedTeleOp extends LinearOpMode {
             robot.leftBack.setPower((y + x + rx) * speedScalar);
             robot.rightBack.setPower((y + x - rx) * speedScalar);
             
+            // -----  -----
+            
             double a = 0.004;
             double b = 0.0;
             
@@ -64,20 +88,78 @@ public class ScreedTeleOp extends LinearOpMode {
             if (robot.limit.isPressed()) armSpeed = Math.min(0, armSpeed);
             
             // if (robot.dist.getDistance(DistanceUnit.CM) < 8) armSpeed = Math.max(-0.2, armSpeed);
-            // if (robot.dist.getDistance(DistanceUnit.CM) < 25 && !gamepad2.left_bumper) armSpeed = Math.min(0.15, armSpeed);
+            if (robot.dist.getDistance(DistanceUnit.CM) < 25 && !gamepad2.left_bumper) armSpeed = Math.min(0.15, armSpeed);
             
             robot.m1.setPower(armSpeed);
             
-            robot.s1.setPosition(grabberOpen ? 0.19 : 0);
+            robot.s1.setPosition(grabberOpen ? 0.38 : 0);
+            // robot.s1.setPosition(grabberOpen ? 0.19 : 0);
+            // robot.s1.setPosition(grabberOpen ? .125 : .2);
             robot.s2.setPosition((intakeToggle / 2.0f) + 0.5f);
             
+            if (armPos != robot.m1.getCurrentPosition())
+            {
+                armPos = robot.m1.getCurrentPosition();
+                wristPos = calcWrist(originPoint - robot.m1.getCurrentPosition());
+            }
+            
+            wristPos += gamepad2.right_stick_y * 0.025;
+            robot.wrist.setPosition(wristPos);
+            
+            if (gamepad2.right_stick_x < -0.5 && !spinPressed) { spinPos++; spinPressed = true; }
+            if (gamepad2.right_stick_x > 0.5 && !spinPressed) { spinPos--; spinPressed = true; }
+            if (gamepad2.right_stick_x > -0.25 && gamepad2.right_stick_x < 0.25) spinPressed = false;
+            
+            if (spinPos >= spinPositions.length) spinPos = spinPositions.length - 1;
+            if (spinPos <= 0) spinPos = 0;
+            
+            if ((originPoint - robot.m1.getCurrentPosition()) <= 2600) {
+                spinPos = 0;
+                isArmUp = false;
+            }
+            
+            if ((originPoint - robot.m1.getCurrentPosition()) >= 2600 && !isArmUp) {
+                spinPos = spinPositions.length - 1;
+                isArmUp = true;
+            }
+            
+            robot.spinner.setPosition(spinPositions[spinPos]);
+            
+            // ----- Drone -----
             if (gamepad1.x && gamepad2.x) robot.droneLauncher.setPosition(1);
             
             if (gamepad2.a || gamepad1.a) robot.droneLauncher.setPosition(0);
             
+            // ----- Lifting mechanism -----
+            if (enableLifting) {
+                if (gamepad1.dpad_up) // go up
+                {
+                    robot.lifter.setPower(0.45);
+                    robot.winch.setPower(-1);
+                } else if (gamepad1.dpad_down) {
+                    robot.lifter.setPower(0.05);
+                    robot.winch.setPower(1);
+                } else {
+                    robot.lifter.setPower(0.1);
+                    robot.winch.setPower(0.01);
+                }
+            } else {
+                robot.lifter.setPower(0);
+                robot.winch.setPower(0);
+            }
+            
             // Toggle Buttons
             
-            if (gamepad2.b && grabberPressed == false)
+            if (wristPos > 1) wristPos = 1;
+            if (wristPos < 0) wristPos = 0;
+            
+            if (gamepad1.y && !liftToggle)
+            {
+                enableLifting = !enableLifting;
+                liftToggle = true;
+            } else if (!gamepad1.y) liftToggle = false;
+            
+            if (gamepad2.b && !grabberPressed)
             {
                 grabberOpen = !grabberOpen;
                 grabberPressed = true;
@@ -98,10 +180,19 @@ public class ScreedTeleOp extends LinearOpMode {
             
             telemetry.addData("Grabber", grabberOpen);
             telemetry.addData("intakeToggle", intakeToggle);
-            // telemetry.addData("Distance", "%.3f, %.3f", robot.dist.getDistance(DistanceUnit.CM), armSpeed);
+            telemetry.addData("Distance", "%.3f, %.3f", robot.dist.getDistance(DistanceUnit.CM), armSpeed);
             telemetry.addData("Limit", robot.limit.isPressed());
             telemetry.update();
         }
+    }
+    
+    double calcWrist(double x)
+    {
+        double y = a * (x + b) * (x + b) + 0.407569f;
+        
+        if (x <= 1700) y = 0.53;
+        
+        return y;
     }
     
     double lerp(double a, double b, double f)
